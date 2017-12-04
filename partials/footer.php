1cef220c828525e73449fcfed714455e538de918
TheStunnr <thestunnr@gmail.com>
2017-12-04 21:01:11 +0800

    final

---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+	<footer class="container-fluid text-center">
+		<div class="row">
+			<span class="col-xs-12">Copyright &copy; 2017 by The Inceptors</span>
+		</div>
+	</footer>
+
+</body>
+</html>
